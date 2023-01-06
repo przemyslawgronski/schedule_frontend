7b1cc32a043d76ae3989ab599a44d2d7f9921602
Przemek89k <przemekgronski@gmail.com>
2023-01-06 18:08:46 +0100

    controlled form to uncontrolled for performance

--- client/src/containers/public/LoginPage.js
+++ client/src/containers/public/LoginPage.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 import { resetRegistered, login } from "../../features/user";
 import { Navigate } from "react-router-dom";
 
@@ -7,37 +7,31 @@ const LoginPage = () => {
     const dispatch = useDispatch();
     const { loading, isAuthenticated, registered } = useSelector(state => state.user)
 
-    const [formData, setFormData] = useState({
-      email: '',
-      password: '',
-    });
+    const formRef = {
+      email: useRef(null),
+      password: useRef(null)
+    }
 
     useEffect(()=>{
       if(registered) dispatch(resetRegistered());
     }, [dispatch, registered]);
 
-    // Destructure object to variables to use them directly
-    const {email, password} = formData;
-  
-    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
+    if (isAuthenticated) return <Navigate to='/dashboard' />
   
     const onSubmit = e => {
       e.preventDefault();
-
-      dispatch(login({email, password}));
+      dispatch(login({email: formRef.email.current.value, password: formRef.password.current.value}));
     }
-
-    if (isAuthenticated) return <Navigate to='/dashboard' />
 
     return (
       <>
           <h1>Log into your account</h1>
           <form onSubmit={onSubmit}>
             <div>
-              <label htmlFor="email">Email</label>
-              <input type='email' name='email' onChange={onChange} value={email} required />
-              <label htmlFor="password">Password</label>
-              <input type='password' name='password' onChange={onChange} value={password} required />
+              <label htmlFor='email'>Email</label>
+              <input ref={formRef.email} type='email' name='email' required />
+              <label htmlFor='password'>Password</label>
+              <input ref={formRef.password} type='password' name='password' required />
             </div>
             { loading ? ("loading") : (<button> Login </button>) }
           </form>
