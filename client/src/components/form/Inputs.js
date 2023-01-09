e28fbdc952b04db8d9b1c3a6a442577cce0866bc
Przemek89k <przemekgronski@gmail.com>
2023-01-09 22:17:54 +0100

    Text component added to form>Inputs

--- client/src/components/form/Inputs.js
+++ client/src/components/form/Inputs.js
@@ -1,5 +1,12 @@
 import React from "react"
 
+const toCamelCase = (str) => {
+  let result = str.replace(/\b[a-z]/g, char => char.toUpperCase()); // First letter of each word to upper case
+  result = result.replace(/\s+/g, ''); // Remove spaces
+  result = result.replace(/^[A-Z]/g, (char) => char.toLowerCase()); // Big letter at beginnig of string to lower case
+  return result;
+}
+
 export const CheckBox = ({isChecked, changeFunc, name, value, labelText}) => {
 
   return (
@@ -30,6 +37,18 @@ export const Password = React.forwardRef((_, ref) => {
     <>
       <label htmlFor='password'>Password</label>
       <input ref={ref} type='password' name='password' required />
+    </>
+  )
+})
+
+export const Text = React.forwardRef(({label}, ref) => {
+
+  const labelName = toCamelCase(label);
+
+  return (
+    <>
+      <label htmlFor={labelName}>{label}</label>
+      <input ref={ref} type='text' name={labelName} required />
     </>
   )
 })
--- client/src/containers/public/RegisterPage.js
+++ client/src/containers/public/RegisterPage.js
@@ -2,14 +2,15 @@ import { useRef } from "react";
 import { Navigate } from "react-router-dom"; // TODO: Should be useNavigate ??? !!!
 import { useSelector, useDispatch } from "react-redux";
 import { register } from "../../features/user";
+import { Email, Password, Text } from "../../components/form/Inputs";
 
 const RegisterPage = () => {
   const dispatch = useDispatch();
   const {registered, loading} = useSelector(state => state.user);
 
   const formRef = {
-    first_name: useRef(null),
-    last_name: useRef(null),
+    firstName: useRef(null),
+    lastName: useRef(null),
     email: useRef(null),
     password: useRef(null),
   };
@@ -19,8 +20,8 @@ const RegisterPage = () => {
   const onSubmit = e => {
     e.preventDefault();
     dispatch(register({
-      first_name: formRef.first_name.current.value,
-      last_name: formRef.last_name.current.value,
+      first_name: formRef.firstName.current.value,
+      last_name: formRef.lastName.current.value,
       email: formRef.email.current.value,
       password: formRef.password.current.value
     }));
@@ -30,16 +31,10 @@ const RegisterPage = () => {
     <>
         <h1>RegisterPage</h1>
         <form onSubmit={onSubmit}>
-          <div>
-            <label htmlFor="first_name">First Name</label>
-            <input ref={formRef.first_name} type='text' name='first_name' required />
-            <label htmlFor="last_name">Laste Name</label>
-            <input ref={formRef.last_name} type='text' name='last_name' required />
-            <label htmlFor="email">Email</label>
-            <input ref={formRef.email} type='email' name='email' required />
-            <label htmlFor="password">Password</label>
-            <input ref={formRef.password} type='password' name='password' required />
-          </div>
+          <Text ref={formRef.firstName} label='First Name' />
+          <Text ref={formRef.lastName} label='Last Name' />
+          <Email ref={formRef.email} />
+          <Password ref={formRef.password} />
           { loading ? ("loading") : (<button> Register </button>) }
         </form>
     </>
@@ -48,4 +43,4 @@ const RegisterPage = () => {
 
 export default RegisterPage;
 
-// TODO: Wyświetlanie błędów rejestracji i psrawdzanie poprawności wprowadzonych danych na froncie i backendzie
+// TODO: Wyświetlanie błędów rejestracji i sprawdzanie poprawności wprowadzonych danych na froncie i backendzie
