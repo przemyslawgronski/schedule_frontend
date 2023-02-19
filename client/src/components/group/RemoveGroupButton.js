4e965967ad566c6a01844cdc2f46e1643074b6f9
Przemek89k <przemekgronski@gmail.com>
2023-02-19 09:05:04 +0100

    Links updated

--- client/src/components/group/RemoveGroupButton.js
+++ client/src/components/group/RemoveGroupButton.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import useRemoveItem from '../features/customHooks/useRemoveItem';
+import useRemoveItem from '../../features/customHooks/useRemoveItem';
 import { useNavigate } from 'react-router-dom';
-import ErrorList from './ErrorList';
+import ErrorList from '../ErrorList';
 
 const RemoveGroupButton = ({group}) => {
 
