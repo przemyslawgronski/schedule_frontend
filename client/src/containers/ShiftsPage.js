8d3e12d61bf871125e8574a8c2033d63024d64e2
Przemek89k <przemekgronski@gmail.com>
2023-03-07 22:48:47 +0100

    Material UI Icons added and styling

--- client/src/containers/ShiftsPage.js
+++ client/src/containers/ShiftsPage.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import ChooseGroup from '../components/newShift/ChooseGroup'
 import ShiftsLinks from '../components/shifts/ShiftsLinks'
+import AddCircleIcon from '@mui/icons-material/AddCircle'
+import style from '../styles/shiftspage.module.css'
 
 const ShiftsPage = () => {
 
   return (
-    <div>
+    <div className={style.shiftspage}>
       <h1>Zmiany</h1>
-      <Link to="new">Dodaj nowe zmiany</Link>
-
+      <Link to="new">Dodaj nowe zmiany <AddCircleIcon/></Link>
+      
       <ChooseGroup>
         <ShiftsLinks />
       </ChooseGroup>
