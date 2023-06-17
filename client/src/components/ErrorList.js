c4b78a5b2be05379d59ead14c710ac24cae52f36
Przemek89k <przemekgronski@gmail.com>
2023-06-17 11:40:59 +0200

    ErrorList style added

--- client/src/components/ErrorList.js
+++ client/src/components/ErrorList.js
@@ -1,19 +1,19 @@
-// React component that renders unordered list of items otherwise renders children
-
-import React from 'react'
+import style from "../styles/errorlist.module.css"
 
 const ErrorList = ({errors}) => {
     // filter out undefined and null values
     errors = errors.filter(err => err !== undefined && err !== null);
 
     return (
-        <>
-            <p>Wystąpił Błąd:</p>
-            <ul>
-                {errors.map((err, idx) => <li key={idx}>{err}</li>)}
-            </ul>
-            <button onClick={() => window.location.reload()}>Odśwież stronę</button>
-        </>
+        <div className={style.errorlist}>
+            <div>
+                <b>Wystąpił Błąd:</b>
+                <ul>
+                    {errors.map((err, idx) => <li key={idx}>{err}</li>)}
+                </ul>
+                <button onClick={() => window.location.reload()}>Odśwież stronę</button>
+            </div>
+        </div>
     )
 }
 
