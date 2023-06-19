f67af0ce575f39b07e34602087eb9ca26418eb92
Przemek89k <przemekgronski@gmail.com>
2023-06-19 12:35:50 +0200

    unnecessary component removed

--- client/src/components/ChooseDaysOffForm.js
+++ client/src/components/ChooseDaysOffForm.js
@@ -1,6 +1,6 @@
-import ShiftsTableCheck from "./ShiftsTableCheck";
 import ChoosedDaysOff from "./newShift/ChoosedDaysOff";
 import CheckDataRows from './newShift/CheckDataRows';
+import ShiftsTable from "./ShiftsTable";
 
 const ChooseDaysOffForm = ({employees, daysOff, handleDaysOff}) => {
 
@@ -11,9 +11,9 @@ return (
     <>
         <p>Wybierz dni wolne:</p>
 
-        <ShiftsTableCheck headers={headers}>
+        <ShiftsTable headers={headers}>
           <CheckDataRows daysOff={daysOff} handleDaysOff={handleDaysOff} headers={headers}/>
-        </ShiftsTableCheck>
+        </ShiftsTable>
 
         <ChoosedDaysOff daysOff={daysOff} employees={employees}/>
     </>
