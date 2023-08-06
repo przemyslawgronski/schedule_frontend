029bd7cfd9de3abed27023f65d50a46d17a51f61
Przemek89k <przemekgronski@gmail.com>
2023-08-06 06:50:47 +0200

    modal button styling

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,14 +1,16 @@
 import { useRef } from 'react'
 import style from './Modal.module.css'
+import OpenInFullIcon from '@mui/icons-material/OpenInFull';
+import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
 
 const Modal = ({children}) => {
     const dialogRef = useRef(null);
 
   return (
     <div className={style.modal}>
-        <button onClick={()=>dialogRef.current.showModal()}>Open Modal</button>
+        <button onClick={()=>dialogRef.current.showModal()}>Powiększ<OpenInFullIcon/></button>
         <dialog ref={dialogRef}>
-            <button onClick={()=>dialogRef.current.close()}>Close Modal</button>
+            <button onClick={()=>dialogRef.current.close()}>Zamknij<CloseFullscreenIcon/></button>
             <div>{children}</div>
         </dialog>
     </div>
