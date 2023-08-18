ae4343ecc84cb80b0d6e14ad1287dd05822c3f70
Przemek89k <przemekgronski@gmail.com>
2023-08-18 20:42:30 +0200

    TODO added

--- client/src/components/TbodyShifts.js
+++ client/src/components/TbodyShifts.js
@@ -1,6 +1,14 @@
 import { unArr } from '../features/utils/arrayUtils'
 
 const TbodyShifts = ({shifts, headers}) => {
+
+  console.log({shifts, headers})
+  // TODO: Important! - this is not working properly
+  // Warning: Encountered two children with the same key, `2023-08-01`.
+  // Keys should be unique so that components maintain their identity across updates.
+  // Non-unique keys may cause children to be duplicated and/or omitted
+  // — the behavior is unsupported and could change in a future version.
+
   return (
     <tbody>
         {shifts.map((shift)=>(
