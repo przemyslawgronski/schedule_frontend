ae668cfbfe8d44091ce71992ad586627e59642d7
Przemek89k <przemekgronski@gmail.com>
2023-09-06 11:18:03 +0200

    Double colon removed

--- client/src/components/employee/EmployeeData.js
+++ client/src/components/employee/EmployeeData.js
@@ -3,13 +3,13 @@ import DisplayObject from "../DisplayObject";
 const EmployeeData = ({employee, groups, spanTag, removeInfo}) => {
 
   const empData = {
-    'Imię:': employee?.first_name || 'Ładowanie...',
-    'Nazwisko:': employee?.last_name || 'Ładowanie...',
-    'Grupy:': employee?.groups?.map?.((group_id)=>
+    'Imię': employee?.first_name || 'Ładowanie...',
+    'Nazwisko': employee?.last_name || 'Ładowanie...',
+    'Grupy': employee?.groups?.map?.((group_id)=>
         <span key={group_id}>{groups?.find(group=>group.id===group_id)?.group_name} </span>
     ) || 'Ładowanie...',
-    'Ostatnia zmiana:': employee?.updated || 'Ładowanie...',
-    'Ukryty:': employee?.hide === undefined ? 'Ładowanie...' : employee.hide ? 'Tak' : 'Nie',
+    'Ostatnia zmiana': employee?.updated || 'Ładowanie...',
+    'Ukryty': employee?.hide === undefined ? 'Ładowanie...' : employee.hide ? 'Tak' : 'Nie',
     }
 
     if(removeInfo && Array.isArray(removeInfo) && removeInfo.length > 0){
