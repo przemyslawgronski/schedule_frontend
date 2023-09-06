d1b302702ce82ba6c61e8003bd94824d78046c2c
Przemek89k <przemekgronski@gmail.com>
2023-09-06 11:10:16 +0200

    'Ładowanie' message added

--- client/src/components/employee/EmployeeData.js
+++ client/src/components/employee/EmployeeData.js
@@ -3,13 +3,13 @@ import DisplayObject from "../DisplayObject";
 const EmployeeData = ({employee, groups, spanTag, removeInfo}) => {
 
   const empData = {
-    'Imię:': employee?.first_name,
-    'Nazwisko:': employee?.last_name,
+    'Imię:': employee?.first_name || 'Ładowanie...',
+    'Nazwisko:': employee?.last_name || 'Ładowanie...',
     'Grupy:': employee?.groups?.map?.((group_id)=>
         <span key={group_id}>{groups?.find(group=>group.id===group_id)?.group_name} </span>
-    ),
-    'Ostatnia zmiana:': employee?.updated,
-    'Ukryty:': employee?.hide ? 'tak' : 'nie'
+    ) || 'Ładowanie...',
+    'Ostatnia zmiana:': employee?.updated || 'Ładowanie...',
+    'Ukryty:': employee?.hide === undefined ? 'Ładowanie...' : employee.hide ? 'Tak' : 'Nie',
     }
 
     if(removeInfo && Array.isArray(removeInfo) && removeInfo.length > 0){
