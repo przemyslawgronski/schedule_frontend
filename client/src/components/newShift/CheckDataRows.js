b4e11de17235d0abcdb588f1cb9d77f3d59c2433
Przemek89k <przemekgronski@gmail.com>
2023-10-13 05:40:08 +0200

    render directly in jsx

--- client/src/components/newShift/CheckDataRows.js
+++ client/src/components/newShift/CheckDataRows.js
@@ -2,29 +2,25 @@ import { CheckBox } from "../form/Inputs"
 
 const CheckDataRows = ({daysOff, handleDaysOff, headers}) => {
 
-  const tableToRender = [];
-
-  daysOff.forEach((empIdToDayOff, date)=>{
-    tableToRender.push(
-      <tr key={date}>
-        <td>{date}</td>
-        {
-          headers.map(emp=>( // Keep order of headers
-            <td key={emp.id}>
-              <CheckBox
-                isChecked={empIdToDayOff.get(emp.id)}
-                changeFunc={()=>handleDaysOff(date, emp.id)}
-              />
-            </td>
-          ))
-        }
-      </tr>
-    )
-  })
-
   return (
     <tbody>
-      {tableToRender}
+      {
+        Array.from(daysOff, ([date, empIdToDayOff]) => { return (
+          <tr key={date}>
+            <td>{date}</td>
+            {
+              headers.map(emp => ( // Keep order of headers
+                <td key={emp.id}>
+                  <CheckBox
+                    isChecked={empIdToDayOff.get(emp.id)}
+                    changeFunc={()=>handleDaysOff(date, emp.id)}
+                  />
+                </td>
+              ))
+            }
+          </tr>
+        )})
+      }
     </tbody>
   )
 }
